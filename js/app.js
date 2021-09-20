4ae94b492079b3350851370c8895d35697fc396f
BSEF19A525 <Bsef19a525@pucit.edu.pk>
2021-09-20 16:06:46 +0500

    Coffee Website Template

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var SideMenuBar = document.getElementById("SideMenuBar");
+function showSideMenuBar(){
+    SideMenuBar.style.left= "0%";
+    SideMenuBar.style.display= "block";
+}
+function hideSideMenuBar(){
+    SideMenuBar.style.left= "-150%";
+}
